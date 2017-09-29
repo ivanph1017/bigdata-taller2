d53407dfff0a76220ae27062ebdd8b4acc012a04
waltersteven <wltrst97@gmail.com>
2017-09-29 18:08:32 -0500

    final

--- src/main/scala/ulima/edu/pe/App.scala
+++ src/main/scala/ulima/edu/pe/App.scala
@@ -19,8 +19,25 @@ object App {
   }
 
   def main(args : Array[String]) {
+
+    //Arqueros
     getGKs()
+
+    //Defensas
     getCBs()
+    getRWBs()
+    getLBs()
+    getLWBs()
+
+    //Mediocampistas
+    getCDMs()
+    getCAMs()
+    getLMs()
+    getRMs()
+
+    //Delanteros
+    getCFs()
+    getSTs()
   }
 
   def getGKs() {
@@ -31,7 +48,7 @@ object App {
   }
 
   def getGKStream() : RDD[Array[String]] = {
-    getSelectablePlayersRDD().filter( fields => fields(15).toUpperCase == "GK" )
+    getSelectablePlayersRDD().filter( fields => fields(15).toUpperCase.contains("GK"))
             //campos a filtrar
             .filter( fields => fields(0) != "" && fields(14) != "" &&
                                fields(25) != "" && fields(48) != "" &&
@@ -49,14 +66,14 @@ object App {
   }
 
   def getCBs() {
-    val max4 = getCBStream().take(4) //Porque son 4 jugadores de esta posicion
+    val max4 = getCBStream().take(2) //Porque son 4 jugadores de esta posicion
     MySparkContext.getSparkContext().parallelize(max4)
       .map( fields => fields(0) + "," + fields(1) )
       .saveAsTextFile("data/resultadoCB/") //folder donde se guarda data/resultado##
   }
 
   def getCBStream() : RDD[Array[String]] = {
-    getSelectablePlayersRDD().filter( fields => fields(15).toUpperCase == "CB" )
+    getSelectablePlayersRDD().filter( fields => fields(15).toUpperCase.contains("CB"))
             //campos a filtrar
             .filter( fields => fields(0) != "" && fields(21) != "" &&
                                fields(22) != "" && fields(23) != "" &&
@@ -75,6 +92,241 @@ object App {
             .sortBy( fields => fields(1).toDouble, ascending = false )
   }
 
+  def getRWBs() {
+    val max4 = getRWBStream().take(2) //Porque son 4 jugadores de esta posicion
+    MySparkContext.getSparkContext().parallelize(max4)
+      .map( fields => fields(0) + "," + fields(1) )
+      .saveAsTextFile("data/resultadoRWB/") //folder donde se guarda data/resultado##
+  }
+
+  def getRWBStream() : RDD[Array[String]] = {
+    getSelectablePlayersRDD().filter( fields => fields(15).toUpperCase.contains("RWB"))
+            //campos a filtrar
+            .filter( fields => fields(0) != "" && fields(19) != "" &&
+                               fields(24) != "" && fields(31) != "" &&
+                               fields(32) != "" && fields(21) != "" &&
+                               fields(33) != "" && fields(34) != "" &&
+                               fields(35) != "")
+            /* los mismos campos pero el
+            Array es de (nombre de jugador, String de campos
+            separados por coma)*/
+            .map( fields => Array(fields(0), fields(19) + "," + fields(24) + ","
+                      + fields(31) + "," + fields(32) + "," + fields(21) + ","
+                      + fields(33) + "," + fields(34) + "," + fields(35)) )
+            .map( fields =>  Array(fields(0),
+            MyMath.getRddPercentile( fields(1), 50 ).toString ) )
+            .sortBy( fields => fields(1).toDouble, ascending = false )
+  }
+
+  def getLBs() {
+    val max4 = getLBStream().take(2) //Porque son 2 jugadores de esta posicion
+    MySparkContext.getSparkContext().parallelize(max4)
+      .map( fields => fields(0) + "," + fields(1) )
+      .saveAsTextFile("data/resultadoLB/") //folder donde se guarda data/resultado##
+  }
+
+  def getLBStream() : RDD[Array[String]] = {
+    getSelectablePlayersRDD().filter( fields => fields(15).toUpperCase.contains("LB"))
+            //campos a filtrar
+            .filter( fields => fields(0) != "" && fields(19) != "" &&
+                               fields(24) != "" && fields(31) != "" &&
+                               fields(32) != "" && fields(21) != "" &&
+                               fields(33) != "" && fields(34) != "" &&
+                               fields(35) != "")
+            /* los mismos campos pero el
+            Array es de (nombre de jugador, String de campos
+            separados por coma)*/
+            .map( fields => Array(fields(0), fields(19) + "," + fields(24) + ","
+                      + fields(31) + "," + fields(32) + "," + fields(21) + ","
+                      + fields(33) + "," + fields(34) + "," + fields(35)) )
+            .map( fields =>  Array(fields(0),
+            MyMath.getRddPercentile( fields(1), 50 ).toString ) )
+            .sortBy( fields => fields(1).toDouble, ascending = false )
+  }
+
+  def getLWBs() {
+    val max4 = getLWBStream().take(2) //Porque son 2 jugadores de esta posicion
+    MySparkContext.getSparkContext().parallelize(max4)
+      .map( fields => fields(0) + "," + fields(1) )
+      .saveAsTextFile("data/resultadoLWB/") //folder donde se guarda data/resultado##
+  }
+
+  def getLWBStream() : RDD[Array[String]] = {
+    getSelectablePlayersRDD().filter( fields => fields(15).toUpperCase.contains("LWB"))
+            //campos a filtrar
+            .filter( fields => fields(0) != "" && fields(19) != "" &&
+                               fields(24) != "" && fields(31) != "" &&
+                               fields(32) != "" && fields(21) != "" &&
+                               fields(33) != "" && fields(34) != "" &&
+                               fields(35) != "")
+            /* los mismos campos pero el
+            Array es de (nombre de jugador, String de campos
+            separados por coma)*/
+            .map( fields => Array(fields(0), fields(19) + "," + fields(24) + ","
+                      + fields(31) + "," + fields(32) + "," + fields(21) + ","
+                      + fields(33) + "," + fields(34) + "," + fields(35)) )
+            .map( fields =>  Array(fields(0),
+            MyMath.getRddPercentile( fields(1), 50 ).toString ) )
+            .sortBy( fields => fields(1).toDouble, ascending = false )
+  }
+
+  def getCFs() {
+    val max2 = getCFStream().take(2) //Porque son 2 jugadores de esta posicion para 22 jugadores
+    MySparkContext.getSparkContext().parallelize(max2)
+      .map( fields => fields(0) + "," + fields(1) )
+      .saveAsTextFile("data/resultadoCF/") //folder donde se guarda data/resultado##
+  }
+
+  def getCFStream() : RDD[Array[String]] = {
+    getSelectablePlayersRDD().filter( fields => fields(15).toUpperCase.contains("CF"))
+            //campos a filtrar
+            .filter( fields => fields(0) != "" && fields(19) != "" &&
+                               fields(26) != "" && fields(34) != "" &&
+                               fields(33) != "" && fields(37) != "" &&
+                               fields(38) != "" && fields(39) != "" &&
+                               fields(40) != "" && fields(42) != "" &&
+                               fields(18) != "" && fields(20) != "")
+             /* los mismos campos pero el
+             Array es de (nombre de jugador, String de campos
+             separados por coma)*/
+            .map( fields => Array(fields(0), fields(19) + "," + fields(26) + ","
+                      + fields(34) + "," + fields(33) + "," + fields(37) + ","
+                      + fields(38) + "," + fields(39) + "," + fields(40) + ","
+                      + fields(42) + "," + fields(18) + "," + fields(20) + ",") )
+            .map( fields =>  Array(fields(0),
+            MyMath.getRddPercentile( fields(1), 50 ).toString ) )
+            .sortBy( fields => fields(1).toDouble, ascending = false )
+  }
+
+  def getSTs() {
+    val max2 = getSTStream().take(2) //Porque son 2 jugadores de esta posicion para 22 jugadores
+    MySparkContext.getSparkContext().parallelize(max2)
+      .map( fields => fields(0) + "," + fields(1) )
+      .saveAsTextFile("data/resultadoST/") //folder donde se guarda data/resultado##
+  }
+
+  def getSTStream() : RDD[Array[String]] = {
+    getSelectablePlayersRDD().filter( fields => fields(15).toUpperCase.contains("ST"))
+            //campos a filtrar
+            .filter( fields => fields(0) != "" && fields(19) != "" &&
+                               fields(26) != "" && fields(34) != "" &&
+                               fields(33) != "" && fields(37) != "" &&
+                               fields(38) != "" && fields(39) != "" &&
+                               fields(40) != "" && fields(42) != "" &&
+                               fields(18) != "" && fields(20) != "")
+             /* los mismos campos pero el
+             Array es de (nombre de jugador, String de campos
+             separados por coma)*/
+             .map( fields => Array(fields(0), fields(19) + "," + fields(26) + ","
+                       + fields(34) + "," + fields(33) + "," + fields(37) + ","
+                       + fields(38) + "," + fields(39) + "," + fields(40) + ","
+                       + fields(42) + "," + fields(18) + "," + fields(20) + ",") )
+            .map( fields =>  Array(fields(0),
+            MyMath.getRddPercentile( fields(1), 50 ).toString ) )
+            .sortBy( fields => fields(1).toDouble, ascending = false )
+  }
+
+  def getCDMs() {
+    val max4 = getCDMStream().take(2) //Porque son 2 jugadores de esta posicion
+    MySparkContext.getSparkContext().parallelize(max4)
+      .map( fields => fields(0) + "," + fields(1) )
+      .saveAsTextFile("data/resultadoCDM/") //folder donde se guarda data/resultado##
+  }
+
+  def getCDMStream() : RDD[Array[String]] = {
+    getSelectablePlayersRDD().filter( fields => fields(15).toUpperCase.contains("CDM"))
+            //campos a filtrar
+            .filter( fields => fields(0) != "" && fields(27) != "" &&
+                               fields(35) != "" && fields(22) != "" &&
+                               fields(21) != "" && fields(31) != "" &&
+                               fields(19) != "" )
+            /* los mismos campos pero el
+            Array es de (nombre de jugador, String de campos
+            separados por coma)*/
+            .map( fields => Array(fields(0), fields(27) + "," + fields(35) + ","
+                      + fields(22) + "," + fields(21) + "," + fields(31) + ","
+                      + fields(19)) )
+            .map( fields =>  Array(fields(0),
+            MyMath.getRddPercentile( fields(1), 50 ).toString ) )
+            .sortBy( fields => fields(1).toDouble, ascending = false )
+  }
+
+  def getCAMs() {
+    val max4 = getCAMStream().take(2) //Porque son 2 jugadores de esta posicion
+    MySparkContext.getSparkContext().parallelize(max4)
+      .map( fields => fields(0) + "," + fields(1) )
+      .saveAsTextFile("data/resultadoCAM/") //folder donde se guarda data/resultado##
+  }
+
+  def getCAMStream() : RDD[Array[String]] = {
+    getSelectablePlayersRDD().filter( fields => fields(15).toUpperCase.contains("CAM"))
+            //campos a filtrar
+            .filter( fields => fields(0) != "" && fields(27) != "" &&
+                               fields(35) != "" && fields(22) != "" &&
+                               fields(21) != "" && fields(31) != "" &&
+                               fields(19) != "" )
+            /* los mismos campos pero el
+            Array es de (nombre de jugador, String de campos
+            separados por coma)*/
+            .map( fields => Array(fields(0), fields(27) + "," + fields(35) + ","
+                      + fields(22) + "," + fields(21) + "," + fields(31) + ","
+                      + fields(19)) )
+            .map( fields =>  Array(fields(0),
+            MyMath.getRddPercentile( fields(1), 50 ).toString ) )
+            .sortBy( fields => fields(1).toDouble, ascending = false )
+  }
+
+  def getLMs() {
+    val max4 = getLMStream().take(2) //Porque son 2 jugadores de esta posicion
+    MySparkContext.getSparkContext().parallelize(max4)
+      .map( fields => fields(0) + "," + fields(1) )
+      .saveAsTextFile("data/resultadoLM/") //folder donde se guarda data/resultado##
+  }
+
+  def getLMStream() : RDD[Array[String]] = {
+    getSelectablePlayersRDD().filter( fields => fields(15).toUpperCase.contains("LM"))
+            //campos a filtrar
+            .filter( fields => fields(0) != "" && fields(34) != "" &&
+                               fields(33) != "" && fields(19) != "" &&
+                               fields(18) != "" && fields(20) != "" &&
+                               fields(38) != "" && fields(35) != "" &&
+                               fields(26) != "")
+            /* los mismos campos pero el
+            Array es de (nombre de jugador, String de campos
+            separados por coma)*/
+            .map( fields => Array(fields(0), fields(34) + "," + fields(33) + ","
+                      + fields(19) + "," + fields(18) + "," + fields(20) + ","
+                      + fields(38) + "," + fields(35) + "," + fields(26) ) )
+            .map( fields =>  Array(fields(0),
+            MyMath.getRddPercentile( fields(1), 50 ).toString ) )
+            .sortBy( fields => fields(1).toDouble, ascending = false )
+  }
+
+  def getRMs() {
+    val max4 = getRMStream().take(2) //Porque son 2 jugadores de esta posicion
+    MySparkContext.getSparkContext().parallelize(max4)
+      .map( fields => fields(0) + "," + fields(1) )
+      .saveAsTextFile("data/resultadoRM/") //folder donde se guarda data/resultado##
+  }
+
+  def getRMStream() : RDD[Array[String]] = {
+    getSelectablePlayersRDD().filter( fields => fields(15).toUpperCase.contains("RM"))
+            //campos a filtrar
+            .filter( fields => fields(0) != "" && fields(34) != "" &&
+                               fields(33) != "" && fields(19) != "" &&
+                               fields(18) != "" && fields(20) != "" &&
+                               fields(38) != "" && fields(35) != "" &&
+                               fields(26) != "")
+            /* los mismos campos pero el
+            Array es de (nombre de jugador, String de campos
+            separados por coma)*/
+            .map( fields => Array(fields(0), fields(34) + "," + fields(33) + ","
+                      + fields(19) + "," + fields(18) + "," + fields(20) + ","
+                      + fields(38) + "," + fields(35) + "," + fields(26) ) )
+            .map( fields =>  Array(fields(0),
+            MyMath.getRddPercentile( fields(1), 50 ).toString ) )
+            .sortBy( fields => fields(1).toDouble, ascending = false )
+  }
 }
 
 object MySparkContext {
