0f18f0e5e4dca4afadbdaef885fd1012216dbbfa
Ivan Palomares <shades1017@gmail.com>
2017-09-29 15:20:34 -0500

    GK y DB done!

--- src/main/scala/ulima/edu/pe/App.scala
+++ src/main/scala/ulima/edu/pe/App.scala
@@ -20,24 +20,56 @@ object App {
 
   def main(args : Array[String]) {
     getGKs()
+    getCBs()
   }
 
   def getGKs() {
-    val max2 = getGKStream().take(2)
+    val max2 = getGKStream().take(2) //Porque son 2 jugadores de esta posicion
     MySparkContext.getSparkContext().parallelize(max2)
       .map( fields => fields(0) + "," + fields(1) )
-      .saveAsTextFile("data/resultadoGK/")
+      .saveAsTextFile("data/resultadoGK/") //folder donde se guarda data/resultado##
   }
 
   def getGKStream() : RDD[Array[String]] = {
     getSelectablePlayersRDD().filter( fields => fields(15).toUpperCase == "GK" )
+            //campos a filtrar
             .filter( fields => fields(0) != "" && fields(14) != "" &&
                                fields(25) != "" && fields(48) != "" &&
                                fields(49) != "" && fields(50) != "" &&
                                fields(51) != "" && fields(52) != "" )
+             /* los mismos campos pero el
+             Array es de (nombre de jugador, String de campos
+             separados por coma)*/
             .map( fields => Array(fields(0), fields(14) + "," + fields(15) + ","
                       + fields(25) + "," + fields(48) + "," + fields(49) + ","
                       + fields(50) + "," + fields(51) + "," + fields(52) ) )
+            .map( fields =>  Array(fields(0),
+            MyMath.getRddPercentile( fields(1), 50 ).toString ) )
+            .sortBy( fields => fields(1).toDouble, ascending = false )
+  }
+
+  def getCBs() {
+    val max4 = getCBStream().take(4) //Porque son 4 jugadores de esta posicion
+    MySparkContext.getSparkContext().parallelize(max4)
+      .map( fields => fields(0) + "," + fields(1) )
+      .saveAsTextFile("data/resultadoCB/") //folder donde se guarda data/resultado##
+  }
+
+  def getCBStream() : RDD[Array[String]] = {
+    getSelectablePlayersRDD().filter( fields => fields(15).toUpperCase == "CB" )
+            //campos a filtrar
+            .filter( fields => fields(0) != "" && fields(21) != "" &&
+                               fields(22) != "" && fields(23) != "" &&
+                               fields(24) != "" && fields(25) != "" &&
+                               fields(27) != "" && fields(34) != "" &&
+                               fields(36) != "" && fields(39) != "" )
+            /* los mismos campos pero el
+            Array es de (nombre de jugador, String de campos
+            separados por coma)*/
+            .map( fields => Array(fields(0), fields(21) + "," + fields(22) + ","
+                      + fields(23) + "," + fields(24) + "," + fields(25) + ","
+                      + fields(27) + "," + fields(34) + "," + fields(36) + ","
+                      + fields(39) ) )
             .map( fields =>  Array(fields(0),
             MyMath.getRddPercentile( fields(1), 50 ).toString ) )
             .sortBy( fields => fields(1).toDouble, ascending = false )
